d8fe18ecd2277b3613e25703958825269854af8b
antongoncharik <ant.goncharik@gmail.com>
2024-10-19 23:19:07 +0300

    Add some fixes

--- src/email.rs
+++ src/email.rs
@@ -3,23 +3,29 @@ use lettre::{Message, SmtpTransport, Transport};
 use std::env;
 
 pub async fn send(body: String) {
-    let from_mail: String = env::var("FROM_MAIL").unwrap();
+    let from_mail: &str = &env::var("FROM_MAIL").unwrap();
     let host_mail: String = env::var("HOST_MAIL").unwrap();
     let password_mail: String = env::var("PASSWORD_MAIL").unwrap();
 
-    let email = Message::builder()
-        .from(from_mail.parse().unwrap())
-        .to("ant.goncharik.development@gmail.com".parse().unwrap())
-        .subject("Message from RabbitMQ")
-        .body(body)
-        .unwrap();
+    let emails = ["ant.goncharik.development@gmail.com"];
 
-    let creds = Credentials::new(from_mail, password_mail);
+    let value = Credentials::new(from_mail.to_string(), password_mail);
 
-    let mailer = SmtpTransport::relay(&host_mail)
-        .unwrap()
-        .credentials(creds)
-        .build();
+    for email in emails {
+        let email_data = Message::builder()
+            .from(from_mail.parse().unwrap())
+            .to(email.parse().unwrap())
+            .subject("Message from RabbitMQ")
+            .body(body.clone())
+            .unwrap();
 
-    _ = mailer.send(&email);
+        let creds = value.clone();
+
+        let mailer = SmtpTransport::relay(&host_mail)
+            .unwrap()
+            .credentials(creds)
+            .build();
+
+        _ = mailer.send(&email_data);
+    }
 }
