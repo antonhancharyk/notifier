10cc313483eb5a762979d8acd70b2ba459897b4b
antongoncharik <ant.goncharik@gmail.com>
2025-04-01 16:31:17 +0300

    Change tg chat id

--- src/tg.rs
+++ src/tg.rs
@@ -3,7 +3,8 @@ use teloxide::prelude::*;
 pub async fn send(msg: String) {
     let bot = Bot::from_env();
 
-    let ids = vec![457923379, 284411673];
+    // let ids = vec![457923379, 284411673];
+    let ids = vec![-4603798918];
 
     for id in ids {
         _ = bot.send_message(ChatId(id), msg.clone()).await
